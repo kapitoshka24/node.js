32d3cb50deedb05a900a6c30b1643db51d074874
kapitoshka24 <51384027+kapitoshka24@users.noreply.github.com>
2019-10-25 09:59:12 +0300

    editen lab5.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
     "parserOptions": {
         "ecmaVersion": 2018
     },
